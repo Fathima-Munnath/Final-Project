dad3cfa2d8738c60a10af791e5fba6156f45610f
Fathima Munnath <fathimamunnath@gmail.com>
2025-02-19 07:08:46 +0530

    updation

--- client/src/pages/restaurant/AddMenu.jsx
+++ client/src/pages/restaurant/AddMenu.jsx
@@ -5,7 +5,6 @@ export const AddMenu = () => {
     return (
         <main className="container mx-auto px-2">
             <section className="my-8 lg:w-3/4 mx-auto px-1">
-                <h2 className="font-semibold text-center text-2xl my-5 underline">Add new Menu</h2>
                 <AddMenuForm />
             </section>
         </main>
