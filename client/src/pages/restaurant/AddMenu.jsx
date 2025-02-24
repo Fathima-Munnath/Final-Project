d5f1e8cc0c9e3c9235f744c143980836e1de377b
Fathima Munnath <fathimamunnath@gmail.com>
2025-02-24 11:15:07 +0530

    updation

--- client/src/pages/restaurant/AddMenu.jsx
+++ client/src/pages/restaurant/AddMenu.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { axiosInstance } from "../../config/AxiosInstance";
+import { axiosInstance } from "../../config/axiosInstance";
 import { AddMenuForm } from "../../components/restaurant/AddMenuForm";
 
 export const AddMenu = () => {
